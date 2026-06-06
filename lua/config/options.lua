35b50d3129579350dff07f275c5fae95d291cf57
nlev <nick@levesque.io>
2026-06-06 18:12:28 -0400

    better mouse scroll

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,7 +4,8 @@ vim.opt.cursorline = true
 
 vim.opt.showmode = false
 
-vim.opt.mouse = "a"
+vim.opt.mouse = 'a'
+vim.opt.mousescroll = 'ver:1,hor:1'
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
@@ -12,7 +13,7 @@ vim.opt.splitbelow = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = 'yes'
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 
@@ -34,14 +35,14 @@ vim.opt.softtabstop = 2
 vim.opt.tabstop = 2
 
 vim.opt.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
-vim.opt.inccommand = "split"
+vim.opt.inccommand = 'split'
 
 vim.opt.termguicolors = true
 
 vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
+  vim.opt.clipboard = 'unnamedplus'
 end)
 
 vim.opt.showtabline = 1
